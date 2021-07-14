ae5b659db1d03725ccb34833215506a1c0079542
Ilkay Citak <citakilkay@gmail.com>
2021-07-14 13:10:46 +0300

    create and style  navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
+  <NavbarComp/>
 </template>
 
 <script>
-setup() {
-
-}
+import SliderHeader from './components/SliderHeader.vue'
+import NavbarComp from './components/Navbar.vue'
+export default {
+  components: {
+    NavbarComp,
+    SliderHeader
+  },
+  setup() {
+    return {};
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='scss'>
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="navbar">
+    <nav class="navbar__body">
+      <a class="navbar__toggler" v-on:click="openToggler">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+      <div class="navbar__list">
+        <a class="navbar__item -logo">
+          <img src="https://www.vudu.com/content/images/logo.svg" alt=""/>
+        </a>
+        <a class="navbar__item">Movies</a>
+        <a class="navbar__item">TV</a>
+        <a class="navbar__item">Free</a>
+        <a class="navbar__item">My Vudu</a>
+      </div>
+      <div class="navbar__list">
+        <a class="navbar__item">Gift Cards</a>
+        <a class="navbar__item">Redeem</a>
+        <a class="navbar__item">Sign In</a>
+        <a class="navbar__item fas fa-search"></a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const openToggler = (e) => {
+      const navbarLists = document.querySelectorAll(".navbar__list");
+      if (e.target.classList.contains("-active")) {
+        e.target.classList.remove("-active");
+        navbarLists.forEach((navbarList) => {
+          navbarList.classList.remove("-open");
+        });
+      } else {
+        e.target.classList.add("-active");
+        navbarLists.forEach((navbarList) => {
+          navbarList.classList.add("-open");
+        });
+      }
+    };
+    return {
+      openToggler,
+    };
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+@import "../assets/scss/navbar";
+</style>
